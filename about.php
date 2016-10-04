aa76e883aabd5731b56c82535ddfd4bf5e564a6b
danlee0306web <leedan030666@yahoo.com.tw>
2016-10-04 23:25:26 +0800

    0.0.0

---
+++ about.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>關於自己</title>
+  <link rel="stylesheet" href="css/font-awesome-4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="js/bxslider/jquery.bxslider.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+<style>
+.flex-control-nav {
+  width:80%;
+  top:860px;
+}
+</style>
+
+</head>
+<body>
+<header id="Header">
+  <div class="Container">
+    <div id="MenuIcon" class="BGEdit"><a class="DB" href="#"><span class="SrOnly">手機版主導覽按鈕</span></a></div>
+    <h1 id="Logo" class="BGEdit"><a class="DB" href="index.php"><span class="SrOnly">Cake House</span></a></h1>
+    <!-- 會員相關區塊 start-->
+    <div id="MemberContent">
+      <div id="MemberIcon" class="BGEdit"><a href="#" class="DB"><span class="SrOnly">會員登入</span></a></div>
+      <div id="CartIcon" class="BGEdit"><a href="#" class="DB"><span class="SrOnly">購物車</span></a></div>
+    </div>
+    <!-- 會員相關區塊 end-->
+    <h3 class="SrOnly">主導覽</h3>
+    <nav id="PrimaryNavigation" class="switch">
+      <ul>
+        <li id="NewsBtn"><a href="javascript:void(0)" class="DB"><span>最新產品</span></a></li>
+        <li id="ProductBtn"><a href="#" class="DB"><span>產品介紹</span></a></li>
+        <li><a href="#" class="DB"><span>好吃的秘訣</span></a></li>
+        <li><a href="#" class="DB"><span>聯絡方式</span></a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<div id="News">
+  <div class="Container">
+    <h4></h4>
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <p class="NewsDate">2016/08/06</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <p class="NewsDate">2016/13</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <p class="NewsDate">2016/08/20</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <p class="NewsDate">2016/08/27</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <p class="NewsDate">2016/09/03</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+
+        <p class="NewsDate">2016/09/10</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+
+  </div>
+</div>
+
+<!-- 頁尾 -->
+<footer id="Footer">
+  <div class="Container">
+    <div id="ContactContent">
+      <div id="FooterLogo"><a href="index.php"><span class="SrOnly">CakeHouse</span></a></div>
+      <p>桃園市大溪區石園路63巷62號2F</p>
+      <p>聯絡專線:0987-267-852</p>
+    </div>
+    <div id="SocietyContent">
+      <div id="FacebookIcon"><a href="" class="DB"><i class="fa fa-facebook-square" aria-hidden="true"></i><span class="SrOnly">Facebook</span></a></div>
+      <div id="GooglePlus"><a href="" class="DB"><i class="fa fa-google-plus-square" aria-hidden="true"></i><span class="SrOnly">Google+</span></a></div>
+    </div>
+  </div>
+  <div id="Copyrights"><p>Copyright © 2016 CakeHouse. All Rights Reserved.</p></div>
+</footer>
+<div id="PageTop"><a class="DB" href="javascript:void(0)">^<span class="SrOnly">回到最上方</span></a></div>
+</div>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/bxslider/jquery.bxslider.js"></script>
+<script src="js/script.js" charset="utf-8"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Dan's Bakery</title>
+  <link rel="stylesheet" href="css/font-awesome-4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="js/bxslider/jquery.bxslider.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+<style>
+.flex-control-nav {
+  width:80%;
+  top:860px;
+}
+</style>
+
+</head>
+<body>
+<header id="Header">
+  <div class="Container">
+    <div id="MenuIcon" class="BGEdit"><a class="DB" href="#"><span class="SrOnly">手機版主導覽按鈕</span></a></div>
+    <h1 id="Logo" class="BGEdit"><a class="DB" href="index.php"><span class="SrOnly">Cake House</span></a></h1>
+    <h2 class="SrOnly">美味甜點新主張！綿密口感，甜在心頭超好吃！</h2>
+    <!-- 會員相關區塊 start-->
+    <div id="MemberContent">
+      <div id="MemberIcon" class="BGEdit"><a href="#" class="DB"><span class="SrOnly">會員登入</span></a></div>
+      <div id="CartIcon" class="BGEdit"><a href="#" class="DB"><span class="SrOnly">購物車</span></a></div>
+    </div>
+    <!-- 會員相關區塊 end-->
+    <h3 class="SrOnly">主導覽</h3>
+    <nav id="PrimaryNavigation" class="switch">
+      <ul>
+        <li id="NewsBtn"><a href="javascript:void(0)" class="DB"><span>最新產品</span></a></li>
+        <li id="ProductBtn"><a href="#" class="DB"><span>產品介紹</span></a></li>
+        <li><a href="#" class="DB"><span>好吃的秘訣</span></a></li>
+        <li><a href="about.php" class="DB"><span>聯絡方式</span></a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<div id="Banner">
+  <div id="BannerLeft"><img src="images/bg/banner_left_bg.png" alt=""></div>
+  <div id="BannerContent">
+    <ul class="bxslider">
+			    <li>
+			      <img src="uploads/banner/banner1.jpg" />
+			    </li>
+			    <li>
+			      <img src="uploads/banner/banner2.jpg" />
+			    </li>
+			    <li>
+			      <img src="uploads/banner/banner3.jpg" />
+			    </li>
+			    <li>
+			      <img src="uploads/banner/banner4.jpg" />
+			    </li>
+          <li>
+			      <img src="uploads/banner/banner5.jpg" />
+			    </li>
+			  </ul>
+  </div>
+  <div id="BannerRight"><img src="images/bg/banner_right_bg.png" alt=""></div>
+
+</div>
+
+<div id="News">
+  <div class="Container">
+    <h4>最新產品</h4>
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_01.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/08/06</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_02.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/13</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_03.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/08/20</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_04.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/08/27</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_05.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/09/03</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+    <!-- 新聞區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="uploads/news/news_06.jpg" alt="" class="NewsImg">
+        <p class="NewsDate">2016/09/10</p>
+        <p class="NewsTitle"><a href="#"></a></p>
+      </div>
+    </div>
+    <!-- 新聞區塊 end-->
+
+  </div>
+</div>
+<div id="Product" class="Container">
+  <h4 class="SrOnly">產品介紹</h4>
+  <!-- 產品區塊 end-->
+  <div class="PCContent">
+    <div class="PCImgContent"><img src="uploads/product_category/product_category_01.jpg" alt=""></div>
+    <div class="PCDecription">
+      <div class="TopLine"></div>
+      <p>手工餅乾</p>
+      <div class="BottomLine"></div>
+    </div>
+  </div>
+  <!-- 產品區塊 end-->
+  <!-- 產品區塊 end-->
+  <div class="PCContent">
+    <div class="PCImgContent PCImgRight"><img src="uploads/product_category/product_category_02.jpg" alt=""></div>
+    <div class="PCDecription">
+    <div class="TopLine"></div>
+    <p>杯子蛋糕</p>
+    <div class="BottomLine"></div>
+  </div>
+  </div>
+  <!-- 產品區塊 end-->
+  <!-- 產品區塊 end-->
+  <div class="PCContent">
+    <div class="PCImgContent"><img src="uploads/product_category/product_category_03.jpg" alt=""></div>
+    <div class="PCDecription">
+    <div class="TopLine"></div>
+    <p>其他甜點</p>
+    <div class="BottomLine"></div>
+  </div>
+  </div>
+  <!-- 產品區塊 end-->
+</div>
+<div id="Taste">
+  <div class="Container">
+    <h4>好吃的秘訣</h4>
+    <!-- 秘訣區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="images/taste/taste_01.jpg" alt="" >
+        <div class="TipTag"><img src="images/icon/icon_tip_01.png" alt=""></div>
+        <p>嚴選食材新鮮、實在</p>
+
+      </div>
+    </div>
+    <!-- 秘訣區塊 end-->
+    <!-- 秘訣區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="images/taste/taste_02.jpg" alt="" >
+        <div class="TipTag"><img src="images/icon/icon_tip_02.png" alt=""></div>
+        <p>細砂糖</p>
+
+      </div>
+    </div>
+    <!-- 秘訣區塊 end-->
+    <!-- 秘訣區塊 start-->
+    <div class="Content">
+      <div class="Wrap">
+        <img src="images/taste/taste_03.jpg" alt="" >
+        <div class="TipTag"><img src="images/icon/icon_tip_03.png" alt=""></div>
+        <p>低筋麵粉</p>
+
+      </div>
+    </div>
+    <!-- 秘訣區塊 end-->
+  </div>
+</div>
+<!-- 頁尾 -->
+<footer id="Footer">
+  <div class="Container">
+    <div id="ContactContent">
+      <div id="FooterLogo"><a href="index.php"><span class="SrOnly">CakeHouse</span></a></div>
+      <p>桃園市大溪區石園路63巷62號2F</p>
+      <p>聯絡專線:0987-267-852</p>
+    </div>
+    <div id="SocietyContent">
+      <div id="FacebookIcon"><a href="" class="DB"><i class="fa fa-facebook-square" aria-hidden="true"></i><span class="SrOnly">Facebook</span></a></div>
+      <div id="GooglePlus"><a href="" class="DB"><i class="fa fa-google-plus-square" aria-hidden="true"></i><span class="SrOnly">Google+</span></a></div>
+    </div>
+  </div>
+  <div id="Copyrights"><p>Copyright © 2016 CakeHouse. All Rights Reserved.</p></div>
+</footer>
+<div id="PageTop"><a class="DB" href="javascript:void(0)">^<span class="SrOnly">回到最上方</span></a></div>
+</div>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/bxslider/jquery.bxslider.js"></script>
+<script src="js/script.js" charset="utf-8"></script>
+
+</body>
+</html>
